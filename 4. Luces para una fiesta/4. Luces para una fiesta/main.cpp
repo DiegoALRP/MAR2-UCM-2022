de8ca5ec00effb8900ffcaec3bf2d8a275b8a03f
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-06 12:33:26 +0100

    4. Luces para una fiesta - Start

---
+++ 4. Luces para una fiesta/4. Luces para una fiesta/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  4. Luces para una fiesta
+//
+//  Created by Diego Alejandro Rodriguez Pereira on 6/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
