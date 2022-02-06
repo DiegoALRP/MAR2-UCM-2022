b07eb975376ba11036aff6f63b16523be646742e
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-06 13:08:43 +0100

    4. Luces para una fiesta

--- 4. Luces para una fiesta/4. Luces para una fiesta/main.cpp
+++ 4. Luces para una fiesta/4. Luces para una fiesta/main.cpp
@@ -1,14 +1,43 @@
-//
-//  main.cpp
-//  4. Luces para una fiesta
-//
-//  Created by Diego Alejandro Rodriguez Pereira on 6/2/22.
-//
+
+// Nombre y Apellidos
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+   
+   // leer los datos de la entrada
+    
+    
+   
+   if (!cin)  // fin de la entrada
+      return false;
+   
+   // escribir sol
+   
+   return true;
+}
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("casos.txt");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   system("PAUSE");
+#endif
+   return 0;
 }
