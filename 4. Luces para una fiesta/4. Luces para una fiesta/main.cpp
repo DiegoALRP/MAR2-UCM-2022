4445cdfd621fb5ad788d8a22c7f45d700592c833
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-08 09:07:19 +0100

    4. Correct!

--- 4. Luces para una fiesta/4. Luces para una fiesta/main.cpp
+++ 4. Luces para una fiesta/4. Luces para una fiesta/main.cpp
@@ -6,8 +6,49 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+const int MAX_INT = 99999;
+
+void resuelve(int num_bombillas, int max_potencia, int min_potencia, vector<int> v_potencia, vector<int> v_coste, vector<int> matriz) {
+    
+    matriz[0] = 0;
+    
+    for (int i = 0; i < num_bombillas; i++) {
+        
+        for (int j = v_potencia[i]; j <= max_potencia; j++) {
+            
+            if (matriz[j - v_potencia[i]] != MAX_INT && matriz[j] > matriz[j - v_potencia[i]] + v_coste[i]) {
+                
+                matriz[j] = matriz[j - v_potencia[i]] + v_coste[i];
+            }
+        }
+    }
+    
+    int min = matriz[min_potencia];
+    int pos = min_potencia;
+    
+    for (int i = min_potencia; i <= max_potencia; i++) {
+        
+        if (matriz[i] < min) {
+            
+            min = matriz[i];
+            pos = i;
+        }
+    }
+    
+    if (matriz[pos] != MAX_INT) {
+        
+        cout << min << " " << pos << endl;
+    }
+    else {
+        
+        cout << "IMPOSIBLE" << endl;
+    }
+    
+}
 
 // resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
@@ -15,10 +56,35 @@ bool resuelveCaso() {
    
    // leer los datos de la entrada
     
+    int num_bombillas;
+    int max_potencia;
+    int min_potencia;
     
+    cin >> num_bombillas;
+    cin >> max_potencia;
+    cin >> min_potencia;
    
    if (!cin)  // fin de la entrada
       return false;
+    
+    
+    vector<int> v_potencia = vector<int>(num_bombillas, 0);
+    vector<int> v_coste = vector<int>(num_bombillas, 0);
+    
+    for (int i = 0; i < num_bombillas; i++) {
+        
+        cin >> v_potencia[i];
+    }
+    
+    for (int i = 0; i < num_bombillas; i++) {
+        
+        cin >> v_coste[i];
+    }
+    
+    
+    vector<int> matriz(max_potencia + 1, MAX_INT);
+    
+    resuelve(num_bombillas, max_potencia, min_potencia, v_potencia, v_coste, matriz);
    
    // escribir sol
    
