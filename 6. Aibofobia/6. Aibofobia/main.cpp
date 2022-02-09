3f5c2e178a785c2786326dc60980df0e491fd8f2
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-09 18:47:59 +0100

    6. Pensarlo Bien

---
+++ 6. Aibofobia/6. Aibofobia/main.cpp
@@ -0,0 +1,77 @@
+
+// Nombre y Apellidos
+// Diego Alejandro
+// Rodriguez Pereira
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX_ENT = 999999;
+
+int aibo(int i, int j, string palabra, vector<vector<int>> matriz) {
+    
+    if (i == j || i > j) {
+        
+        matriz[i][j];
+        return 0;
+    }
+    if (palabra[i] == palabra[j]) {
+        
+        return aibo(i + 1, j - 1, palabra, matriz);
+    }
+    else {
+        
+        matriz[i][j] = min(aibo(i, j - 1, palabra, matriz), aibo(i + 1, j, palabra, matriz)) + 1;
+        
+        return matriz[i][j];
+    }
+}
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+   
+   // leer los datos de la entrada
+    
+    string palabra;
+    
+    cin >> palabra;
+   
+   if (!std::cin)  // fin de la entrada
+      return false;
+    
+    int long_palabra = palabra.size();
+    
+    vector<vector<int>> matriz(long_palabra, vector<int>(long_palabra, MAX_ENT));
+   
+    int res = aibo(0, long_palabra - 1, palabra, matriz);
+    cout << matriz[ << endl;
+   // escribir sol
+   
+   return true;
+}
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("casos.txt");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   system("PAUSE");
+#endif
+   return 0;
+}
