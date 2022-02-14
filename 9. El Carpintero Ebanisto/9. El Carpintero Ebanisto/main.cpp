b64fec1e4708cd3b69da01fc283443d0c191bc94
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-14 16:31:01 +0100

    9. Da bien, pero tengo un mal acceso

--- 9. El Carpintero Ebanisto/9. El Carpintero Ebanisto/main.cpp
+++ 9. El Carpintero Ebanisto/9. El Carpintero Ebanisto/main.cpp
@@ -1,14 +1,81 @@
-//
-//  main.cpp
-//  9. El Carpintero Ebanisto
-//
-//  Created by Diego Alejandro Rodriguez Pereira on 14/2/22.
-//
+
+// Nombre y Apellidos
+// Diego Alejandro
+// Rodriguez Pereira
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+const int max_int = 99999;
+
+int ayuda_por_favor(int tam_tabla, int num_cortes, const vector<int> &cortes, vector<vector<int>> &esfuerzo) {
+    
+    for (int d = 2; d <= num_cortes + 1; d++) {
+        for (int i = 0; i <= num_cortes + 1 - d; i++) {
+            int j = i + d;
+            esfuerzo[i][j] = max_int;
+            for (int k = i; k < j; k++) {
+                
+                int temp = esfuerzo[i][k] + esfuerzo[k][j] + 2*(cortes[j] - cortes[i]);
+                if (temp < max_int && temp < esfuerzo[i][j]) {
+                    esfuerzo[i][j] = temp;
+                }
+            }
+        }
+    }
+    
+    return esfuerzo[0][num_cortes + 1];
+}
+
+bool resuelveCaso() {
+
+    //leer entrada
+    int tam_tabla, num_cortes;
+    
+    cin >> tam_tabla >> num_cortes;
+   
+    if (tam_tabla == 0 && num_cortes == 0)  // fin de la entrada
+      return false;
+    
+    vector<int> cortes = vector<int>(num_cortes + 2);
+    for (int i = 1; i <= num_cortes; i++) {
+        
+        cin >> cortes[i];
+    }
+    
+    cortes[num_cortes + 1] = tam_tabla;
+    
+    vector<vector<int>> esfuerzo(num_cortes + 2, vector<int> (num_cortes));
+    
+    int res = ayuda_por_favor(tam_tabla, num_cortes, cortes, esfuerzo);
+    
+    cout << "res: " << res << endl;
+    cout << res << endl;
+
+    //escribir resultado
+   
+    return true;
+}
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("casos.txt");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   system("PAUSE");
+#endif
+   return 0;
 }
