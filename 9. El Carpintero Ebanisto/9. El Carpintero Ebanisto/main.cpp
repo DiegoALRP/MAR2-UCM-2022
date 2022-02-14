e6e2e8d081312ab9f4c6a77f0caf6b9eb033c469
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-14 15:59:28 +0100

    9. Comienzo

---
+++ 9. El Carpintero Ebanisto/9. El Carpintero Ebanisto/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  9. El Carpintero Ebanisto
+//
+//  Created by Diego Alejandro Rodriguez Pereira on 14/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
