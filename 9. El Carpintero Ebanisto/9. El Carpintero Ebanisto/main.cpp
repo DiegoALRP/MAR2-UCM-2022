14f458618d10632be409823481aa4a78ff7b09c6
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2022-02-16 12:28:56 +0100

    9. Correct!

--- 9. El Carpintero Ebanisto/9. El Carpintero Ebanisto/main.cpp
+++ 9. El Carpintero Ebanisto/9. El Carpintero Ebanisto/main.cpp
@@ -9,29 +9,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "EnterosInf.h"
 
 using namespace std;
 
-const int max_int = 99999;
+const int max_int = Infinito._intInf;
 
 int ayuda_por_favor(int tam_tabla, int num_cortes, const vector<int> &cortes, vector<vector<int>> &esfuerzo) {
     
-    for (int d = 2; d <= num_cortes + 1; d++) {
+    for (int d = 1; d <= num_cortes + 1; d++) {
         for (int i = 0; i <= num_cortes + 1 - d; i++) {
             int j = i + d;
-            esfuerzo[i][j] = max_int;
-            for (int k = i; k < j; k++) {
+            
+            if (j == i + 1) {
                 
-                if (j > num_cortes + 1 || i > num_cortes + 1 || k > num_cortes + 1) {
+                esfuerzo[i][j] = 0;
+            }
+            else {
+                esfuerzo[i][j] = max_int;
+                for (int k = i + 1; k < j; k++) {
                     
-                    cout << "Error i: " << i << endl;
-                    cout << "Error j: " << j << endl;
-                    cout << "Error k: " << k << endl;
-                    continue;
-                }
-                int temp = esfuerzo[i][k] + esfuerzo[k][j] + 2*(cortes[j] - cortes[i]);
-                if (temp < max_int && temp < esfuerzo[i][j]) {
-                    esfuerzo[i][j] = temp;
+                    /*if (j > num_cortes + 1 || i > num_cortes + 1 || k > num_cortes + 1) {
+                        
+                        cout << "Error i: " << i << endl;
+                        cout << "Error j: " << j << endl;
+                        cout << "Error k: " << k << endl;
+                        continue;
+                    }*/
+                    int temp = esfuerzo[i][k] + esfuerzo[k][j] + 2*(cortes[j] - cortes[i]);
+                    //if (temp < max_int && temp < esfuerzo[i][j]) {
+                    if (temp < esfuerzo[i][j]) {
+                        esfuerzo[i][j] = temp;
+                    }
                 }
             }
         }
@@ -50,15 +59,16 @@ bool resuelveCaso() {
     if (tam_tabla == 0 && num_cortes == 0)  // fin de la entrada
       return false;
     
-    vector<int> cortes = vector<int>(num_cortes + 2);
+    vector<int> cortes = vector<int>(num_cortes + 2, 0);
     for (int i = 1; i <= num_cortes; i++) {
         
         cin >> cortes[i];
     }
     
+    cortes[0] = 0;
     cortes[num_cortes + 1] = tam_tabla;
     
-    vector<vector<int>> esfuerzo(num_cortes + 2, vector<int> (num_cortes));
+    vector<vector<int>> esfuerzo(num_cortes + 2, vector<int> (num_cortes + 2, 0));
     
     int res = ayuda_por_favor(tam_tabla, num_cortes, cortes, esfuerzo);
     
