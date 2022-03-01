474b89de968f59915479cea1b23c4ec1c0cc8b8d
DiegoARP <alrp.diego@gmail.com>
2022-03-01 13:22:13 +0100

    13. Inicio

---
+++ 13. Insercion de Parentesis/13. Insercion de Parentesis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  13. Insercion de Parentesis
+//
+//  Created by Diego Alejandro Rodriguez Pereira on 1/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
