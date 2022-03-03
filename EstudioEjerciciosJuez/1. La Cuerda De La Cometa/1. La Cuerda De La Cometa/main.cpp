1c175afdc33190cebd821df5c310d9e1d07e5e73
DiegoARP <alrp.diego@gmail.com>
2022-03-03 14:10:23 +0100

    Comienzo Estudio

---
+++ EstudioEjerciciosJuez/1. La Cuerda De La Cometa/1. La Cuerda De La Cometa/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1. La Cuerda De La Cometa
+//
+//  Created by Diego Alejandro Rodriguez Pereira on 3/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
