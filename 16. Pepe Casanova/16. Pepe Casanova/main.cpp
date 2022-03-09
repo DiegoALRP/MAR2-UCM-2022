2921c3c1494a001b5d51f1aab504857fa85c0cfa
DiegoARP <alrp.diego@gmail.com>
2022-03-09 10:57:26 +0100

    16. Comienzo

---
+++ 16. Pepe Casanova/16. Pepe Casanova/main.cpp
@@ -0,0 +1,43 @@
+
+// Nombre y Apellidos
+// Diego Alejandro
+// Rodriguez Pereira
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+bool resuelveCaso() {
+
+    //leer entrada
+   
+    if (!std::cin)  // fin de la entrada
+      return false;
+
+    //escribir resultado
+   
+    return true;
+}
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("casos.txt");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+   
+   while (resuelveCaso());
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   system("PAUSE");
+#endif
+   return 0;
+}
