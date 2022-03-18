12dd8fe11e4bd0ac1abba3a9aff88f721da7992e
DiegoARP <alrp.diego@gmail.com>
2022-03-18 17:47:19 +0100

    18. Correct! (Missing Optimistic Bound)

--- 18. El Restaurante Come Sano/18. El Restaurante Come Sano/main.cpp
+++ 18. El Restaurante Come Sano/18. El Restaurante Come Sano/main.cpp
@@ -9,9 +9,91 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+const int MIN_DISTANCIA = 2;
+
+struct Nodo {
+	
+	vector<int> sol; //sol[i] contiene la persona i que se ha sentado en la silla j
+	//vector<bool> marcas; //marcas[i] dice si la persona i se ha sentado ya o no
+	int k_persona;
+	int parejas;
+	int parejas_estimado;
+	bool operator<(Nodo const& otro) const {
+		return otro.parejas_estimado > parejas_estimado;
+	}
+};
+
+bool es_valido(const int &n, const int &m, const vector<vector<double>> &distancias, const vector<vector<int>> &allegados, Nodo &X) {
+	
+	bool is_valid = true;
+	int persona_actual = X.k_persona;
+	for (int i = 0; i < persona_actual; i++) {
+		
+		int silla_actual = X.sol[persona_actual];
+		int silla_otro = X.sol[i];
+		
+		if (silla_actual == silla_otro) return false;
+		
+		if (allegados[persona_actual][i] == 0) {
+			if (distancias[silla_actual][silla_otro] < MIN_DISTANCIA) {
+				return false;
+			}
+		}
+		
+		if (allegados[persona_actual][i] == 1) {
+			if (distancias[silla_actual][silla_otro] < MIN_DISTANCIA) {
+				X.parejas++;
+			}
+		}
+	}
+	
+	return is_valid;
+}
+
+void restaurante_rp(const int &n, const int &m, const vector<vector<double>> &distancias, const vector<vector<int>> &allegados, vector<int> &mejor_sol, int &max_parejas) {
+	
+	Nodo Y;
+	Y.sol = vector<int>(n, -1);
+	Y.k_persona = -1;
+	Y.parejas = 0;
+	Y.parejas_estimado = 0;
+	priority_queue<Nodo> cola;
+	cola.push(Y);
+	max_parejas = -1;
+	
+	while (!cola.empty() /*&& cola.top().parejas_estimado > max_parejas*/) {
+		
+		Y = cola.top();
+		cola.pop();
+		Nodo X(Y);
+		X.k_persona++;
+		
+		for (int j = 0; j < n; j++) {
+			
+			X.sol[X.k_persona] = j;
+			X.parejas = Y.parejas;
+			if (es_valido(n, m, distancias, allegados, X)) {
+				
+				//X.parejas_estimado = Y.parejas_estimado;
+				
+				if (X.k_persona == m - 1) {
+					//cout << "prueba: " << X.parejas << " ";
+					if (X.parejas > max_parejas) {
+						mejor_sol = X.sol;
+						max_parejas = X.parejas;
+					}
+				}
+				else {
+					cola.push(X);
+				}
+			}
+		}
+	}
+}
 
 void resuelveCaso() {
 
@@ -33,6 +115,16 @@ void resuelveCaso() {
         }
     }
 
+	vector<int> mejor_sol;
+	int max_parejas = -1;
+	restaurante_rp(n_plazas, m_comensales, distancias, allegados, mejor_sol, max_parejas);
+	
+	if (max_parejas <= 0) {
+		cout << "CANCELA" << endl;
+	}
+	else {
+		cout << "PAREJAS " << max_parejas << endl;
+	}
     //escribir resultado
 }
 
